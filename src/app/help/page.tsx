cce7f1a79e2cb81f080d443beed19f84b1eea854
Rory Hackney <roryhackney@gmail.com>
2025-02-11 12:01:00 -0800

    removed duplicate main tag

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,8 +1,8 @@
 export default function Help() {
     return (
-        <main>
+        <>
             <h1>Help/Search</h1>
             <p>Hello world!</p>
-        </main>
+        </>
     );
 }
