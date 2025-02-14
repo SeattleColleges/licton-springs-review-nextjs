b5a2dec83af01d579dac02bf64e0da9c4a95b252
Nguyen Giang <67605679+nguyengiang2000@users.noreply.github.com>
2025-02-14 13:14:34 -0800

    licton-Spring-review-nextjs-footer-restyling_17_footer-design  (#54)
    * homepage design
    
    * minor structural changes, image file
    
    * Added NSC logo and restyling Footer.
    
    * revert package.json
    
    * revert package-lock.json
    
    * Fixed merge issue where the footer was showing  twice
    
    * fixed import
    
    * fixed Image import
    
    ---------
    
    Co-authored-by: rojeekoju <rojeek2@gmail.com>
    Co-authored-by: Rory Hackney <roryhackney@gmail.com>
    Co-authored-by: Nguyen Giang <nguyengiang.8940@gmail.com>
    Co-authored-by: cshimm <cameronjshimmin@gmail.com>
    Co-authored-by: Marco Bejarano Oseguera <marco.bejarano03@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
-        <Navbar/>      
+        <Navbar/>
+      
         <main>{children}</main>
         <Footer/>
       </body>
--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,8 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <nav className="footer">
+    footer-nguyengiang
+    <nav className="footer">
+      <ul>
+        <li>
+          <img src="/north-seattle-tree-frogs.png" alt="image of NSC Logo" width="160" height="80" />
+        </li>
+        <li>
+          <Link href="/archive">Archive</Link>
+        </li>
+        <li>
+          <Link href="job">Job</Link>
+        </li>
+        <li>
+          <Link href="community page">Community Page</Link>
+        </li>
+        <li>
+          <Link href="help">Help</Link>
+        </li>
+        <li>
+          <Link href="link">Link</Link>
+        </li>
+        <li>
+          <Link href="https://www.getrave.com/login/seattlecolleges">
+          <Image src="/rave.png" alt="image of NSC Logo" width="170" height="40" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+
+      {/* <nav className="footer">
         <ul>
           <li>
             <Link href="/archive">Archive</Link>
@@ -20,7 +50,7 @@ export default function Footer() {
             <Link href="link">Link</Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
     </footer>
   );
 }
