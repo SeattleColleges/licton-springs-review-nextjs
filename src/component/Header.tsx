bb5c565e56ab6718b9f0865c9b4c07ceca52af84
Edale Miguel <edale.miguel@seattlecolleges.edu>
2025-03-09 14:37:34 -0700

    Updated classname to be more descriptive and then created a social media links component for better reusability

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,7 +1,5 @@
-
 import Image from "next/image";
-import Link from "next/link";
-import { BsInstagram, BsTiktok } from "react-icons/bs";
+import SocialMediaLinks from './SocialMediaLinks';
 
 export default function Header() {
     return (
@@ -12,18 +10,7 @@ export default function Header() {
                 width="595" height="90"
                 className="header-logo"
             />
-            {/* Social Media Icons */}
-            <div className="social-icons">
-                <Link href="https://www.instagram.com/lictonspringsreview/" target="_blank" rel="noopener noreferrer">
-                    <BsInstagram />
-                </Link>
-                <Link href="https://www.tiktok.com/@lictonspringsreview/" target="_blank" rel="noopener noreferrer">
-                    <BsTiktok />
-                </Link>
-            </div>
-            <p className="slogan">
-                Visual and literary art from the students, staff, faculty, and alumni of North Seattle College
-            </p>
+            <SocialMediaLinks />
         </header>
     );
 }
