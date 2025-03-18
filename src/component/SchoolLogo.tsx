75023079fe1d6033c7b9594943b6a2f2cb92a090
Nguyen Giang <nguyengiang.8940@gmail.com>
2025-03-17 22:31:48 -0700

    fixed global wrong destination.

--- src/component/SchoolLogo.tsx
+++ src/component/SchoolLogo.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import "./global.css";
+import Image from "next/image";
+import Link from "next/link";
+import "../app/global.css";
 
 const SchoolLogo = () => {
   return (
     <header className="header">
-      <a href="https://northseattle.edu" className="logo-link" target="_blank" rel="noopener noreferrer">
-        <img src="../public/north-seattle-tree-frogs.png" alt="North Seattle College Logo" className="logo" />
-      </a>
+      <Link href="https://northseattle.edu" target="_blank" rel="noopener noreferrer" className="logo-link">
+        <Image 
+          src="/north-seattle-tree-frogs.png" 
+          alt="North Seattle College Logo" 
+          width={150} 
+          height={150} 
+          className="logo"
+        />
+      </Link>
     </header>
   );
 };
