6fb0b8f54f352b226e353240763fc6635563c921
cshimm <cameronjshimmin@gmail.com>
2025-03-22 10:55:05 -0700

    added logoImg variable

--- src/component/SchoolLogo.tsx
+++ src/component/SchoolLogo.tsx
@@ -1,7 +1,7 @@
-
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import logoImg from 'public/NSCMascotLogo.png'
 
 const links = [
   { name: "North Seattle College", url: "https://northseattle.edu/" },
@@ -13,13 +13,17 @@ const links = [
 
 const SchoolLogo = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const logoScale = 0.1;
   return (
     <div className="relative">
       {/* Logo */}
       <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
         <Image
-          src="/NSCMascotLogo.png" alt="NSC Mascot Logo" width={60} height={40} className="cursor-pointer"
+          src={logoImg}
+          alt="NSC Mascot Logo"
+          width={logoImg.width * logoScale}
+          height={logoImg.height * logoScale}
+          className="cursor-pointer"
         />
       </button>
 
