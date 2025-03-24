528837c67a4c281f8d3258baf77919623f5541ae
rojeekoju <146134351+rojeekoju@users.noreply.github.com>
2025-03-23 23:59:17 -0700

    Jobpage content (#91)
    * Jobpage content
    
    * fix container

--- src/app/job/page.tsx
+++ src/app/job/page.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
+
 export default function JobPage() {
-    return (
-      <main>
-        <h1>Job</h1>
-        <p>Hello World!</p>
-      </main>
-    );
-  }
+  return (
+    <main className="job-container">
+      <h1 className="heading">JOBS</h1>
+      <p className="paragraph">
+        The NSC Literary Guild is not currently seeking Editors, but positions sometimes open throughout the year.
+        Editor positions are open to any current NSC students.
+      </p>
+      <p className="paragraph">
+        For more information, please contact Faculty Advisor at{' '}
+        <a href="mailto:jim.jewell@seattlecolleges.edu" className="link">
+          jim.jewell@seattlecolleges.edu
+        </a>.
+      </p>
+    </main>
+  );
+}
