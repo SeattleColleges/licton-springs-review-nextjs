be5d58a2c2d963f95dc283ab3a9b87763466cb5e
Rory Hackney <72114389+roryhackney@users.noreply.github.com>
2025-03-23 20:22:33 -0700

    audio page no posts message (#90)

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -1,8 +1,17 @@
+import { PostsByCategory } from "@/component/PostsByCategory";
+import { getCategoryId } from "@/fetch-category-posts";
+
 export default function Audio() {
-    return (
-        <>
-            <h1>Audio</h1>
-            <p>Hello world!</p>
-        </>
-    );
+    return getCategoryId("Audio")
+    .then((id) => {
+        const content = id === -1 ? 
+                        <p>There are currently no posts for Audio. Submit your work today!</p> :
+                        <PostsByCategory category="Audio"/>
+        return (
+            <main className="audio-container">
+                <h1>Audio</h1>
+                {content}
+            </main>
+        );
+    })
 }
