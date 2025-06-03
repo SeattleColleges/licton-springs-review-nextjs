4ee8ad54122f2a3334b0c112c4770f6ad9a44797
Rory Hackney <roryhackney@gmail.com>
2025-06-02 23:10:21 -0700

    handle new fiction posts with an array initially

--- src/app/fiction/post/page.tsx
+++ src/app/fiction/post/page.tsx
@@ -1,5 +1,13 @@
+"use client";
 import Detail from "@/component/Detail";
+import { useSearchParams } from "next/navigation";
 
 export default function FictionPost() {
-    return <Detail />;
+    const searchParams = useSearchParams();
+    const isWp = searchParams.get("wp");
+    if (isWp === "false") {
+        return <FictionPost />;
+    } else {
+        return <Detail />;
+    }
 }
