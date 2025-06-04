11e35007f5713dcc3c8895bab5e6f846cc2e1198
Rory Hackney <roryhackney@gmail.com>
2025-06-03 17:27:47 -0700

    fiction posts display correctly now

--- src/app/fiction/post/page.tsx
+++ src/app/fiction/post/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Detail from "@/component/Detail";
+import SingleFictionPost from "@/component/SingleFictionPost";
 import { useSearchParams } from "next/navigation";
 
 export default function FictionPost() {
     const searchParams = useSearchParams();
     const isWp = searchParams.get("wp");
     if (isWp === "false") {
-        return <FictionPost />;
+        return <SingleFictionPost />;
     } else {
         return <Detail />;
     }
